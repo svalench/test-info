646995eb22731603e04605d1b1df541ba9e08810
valenchits <8940113Wert>
2020-10-11 16:37:32 +0300

    rewrite en ru text

--- web/forms.py
+++ web/forms.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-
+from django.utils.translation import gettext as _
 
 class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
         self.fields['email'].label = 'Email'
-        self.fields['username'].label = 'Your nickname'
+        self.fields['username'].label = _('Your nickname')
+        self.fields['password1'].label = _('Your password')
         for visible in self.visible_fields():
             visible.field.widget.attrs.update({'class': 'form-control'})
 
