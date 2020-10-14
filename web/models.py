90a6c5ba90e2abc193f186ef0f44bdef63102975
svalench <chitsalex@gmail.com>
2020-10-14 06:16:46 -0700

    update for corparation

--- web/models.py
+++ web/models.py
@@ -6,11 +6,12 @@ class Corparations(models.Model):
     """
     модель для предприятий
     """
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name="who created")
-    name = models.CharField("Name corparation", max_length=255)
+    user_id = models.ForeignKey(User, unique=True, on_delete=models.SET_NULL, null=True, verbose_name="who created")
+    name = models.CharField("Name corparation", max_length=255, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class Depatments(models.Model):
     """
@@ -22,6 +23,7 @@ class Depatments(models.Model):
     def __str__(self):
         return self.name
 
+
 class Roles(models.Model):
     """
     модель с должностями для пользователей
@@ -32,6 +34,7 @@ class Roles(models.Model):
     def __str__(self):
         return self.name
 
+
 class Employees(models.Model):
     """
     модель для сотрудников предприятия в которой ему назначется принадлежность к нему (предприятию), должность
@@ -39,7 +42,7 @@ class Employees(models.Model):
     """
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="user id")
     corp_id = models.ForeignKey(Corparations, on_delete=models.CASCADE, verbose_name="corp id")
-    role_id = models.ForeignKey(Roles, on_delete=models.SET_NULL,null=True, verbose_name="user id")
+    role_id = models.ForeignKey(Roles, on_delete=models.SET_NULL, null=True, verbose_name="user id")
     dep_id = models.ForeignKey(Depatments, on_delete=models.SET_NULL, null=True, verbose_name="department id")
 
     def __str__(self):
--- web/views.py
+++ web/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
 from web.forms import CreateCorparation
@@ -13,17 +13,24 @@ class Cabinet:
         })
     def create_corp(request):
         if request.method == 'POST':
-            form = CreateCorparation(request.POST)
+            try:
+                cor = Corparations.objects.filter(user_id=request.user)
+                form = CreateCorparation(request.POST, instance=cor[0])
+            except:
+                form = CreateCorparation(request.POST)
             if form.is_valid():
                 res = form.save(commit=False)
                 res.user_id = request.user
                 res.save()
                 return HttpResponseRedirect(reverse('cabinet'))
         else:
-            cor = Corparations.objects.filter(user_id=request.user)
-            if(cor[0]):
-                data = {'name':cor[0].name}
-            else:
+            try:
+                cor = Corparations.objects.filter(user_id=request.user)
+                if(cor[0]):
+                    data = {'name':cor[0].name}
+                else:
+                    data = {}
+            except:
                 data = {}
             form = CreateCorparation(initial=data)
         return render(request,'cabinet/corp_create.html',{'form': form})
