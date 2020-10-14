7905576f7f245cb8623ffc9fda56d4b0a2858a41
svalench <chitsalex@gmail.com>
2020-10-14 00:25:18 -0700

    add models add button for create corp

--- web/models.py
+++ web/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Corparations(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name="who created")
+    name = models.CharField("Name corparation", max_length=255)
+
+
+class Depatments(models.Model):
+    corp_id = models.ForeignKey(Corparations, on_delete=models.CASCADE, verbose_name="corp id")
+    name = models.CharField("Name department", max_length=255)
+
+
+class Roles(models.Model):
+    """
+    модель с должностями для пользователей
+    """
+    corp_id = models.ForeignKey(Corparations, on_delete=models.CASCADE, verbose_name="corp id")
+    name = models.CharField("Name role", max_length=255)
+
+
+class Employees(models.Model):
+    """
+    модель для сотрудников предприятия в которой ему назначется принадлежность к нему (предприятию), должность
+    и цех (подразделение)
+    """
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="user id")
+    corp_id = models.ForeignKey(Corparations, on_delete=models.CASCADE, verbose_name="corp id")
+    role_id = models.ForeignKey(Roles, on_delete=models.SET_NULL,null=True, verbose_name="user id")
+    dep_id = models.ForeignKey(Depatments, on_delete=models.SET_NULL, null=True, verbose_name="department id")
